68017dc20bc827850710ca5953e3d81515193729
Maher El Gamil <maherbusnes@gmail.com>
2020-10-22 15:34:32 +0200

    Add SQLALCHEMY_TRACK_MODIFICATIONS as False to avoid significant overhead

--- config.py
+++ config.py
@@ -9,5 +9,5 @@ DEBUG = True
 # Connect to the database
 
 
-# TODO IMPLEMENT DATABASE URL
 SQLALCHEMY_DATABASE_URI = 'postgres://maherelgamil@localhost:5432/fyyur'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
