5f98677db4f54a855b3b5b411d34105035abeed9
Maher El Gamil <maherbusnes@gmail.com>
2020-10-22 15:41:32 +0200

    fix type of state and genres column

--- models.py
+++ models.py
@@ -9,9 +9,9 @@ class Venue(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
-    genres = db.Column(db.String)
+    genres = db.Column(db.ARRAY(db.String))
     city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
+    state =db.Column(db.ARRAY(db.String))
     address = db.Column(db.String(120))
     phone = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
@@ -39,7 +39,7 @@ class Venue(db.Model):
     def serialize(self):
         return {'id': self.id,
                 'name': self.name,
-                'genres': self.genres.split(','),
+                'genres': self.genres,
                 'city': self.city,
                 'state': self.state,
                 'phone': self.phone,
@@ -111,10 +111,10 @@ class Artist(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
+    db.Column(db.ARRAY(db.String))
     address = db.Column(db.String(120))
     phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
+    genres = db.Column(db.ARRAY(db.String))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
     website = db.Column(db.String(120))
